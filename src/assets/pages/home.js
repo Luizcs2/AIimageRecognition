de03bc42995ddaec358c1dabea68fc85b9f42f2b
Luizcs2 <luizohto2012@gmail.com>
2025-03-24 00:11:54 +0000

    added to ui & added file selction

--- src/assets/pages/home.js
+++ src/assets/pages/home.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 
 function Homepage() {
-  const [image, setImage] = useState(1);
-  const [result, setResult] = useState(1);
+  const [image, setImage] = useState(null);
+  const [result, setResult] = useState(null);
 
   const handleImageUpload = (e) => {
     setImage(URL.createObjectURL(e.target.files[0]));
@@ -10,41 +10,47 @@ function Homepage() {
 
   const handlesubmit = async () => {
     const response = await fetch("api/recognize-image", {
-      mthod: "POST",
+      method: "POST",
       body: image,
     });
     const data = await response.json();
-
     setResult(data);
   };
 
   return (
-    <div classname="homepage p-6 justify-center items-center h-screen">
-      <h2 classname="text-white text-x1 mb-4">
-        Upload an Image for AI recongnition
+    <div className="homepage flex flex-col justify-center items-center min-h-screen p-6">
+      <h2 className=" text-blue-500 text-4xl mb-10 text-center h-20">
+        Upload an Image for AI recognition
       </h2>
 
-      <input type="file" onChange={handleImageUpload} classname="mb-4 " />
+      <input
+        classname="file"
+        type="file"
+        onChange={handleImageUpload}
+        className="mb-4 file:bg-blue-500 file:font file:border-blue-500 file:border-0 file:text-white p-8 file:rounded-lg file:p-2 mt-4 border-dotted border-black border-2 rounded-lg"
+        />
 
-      <div className="image-preview mb-4">
-        {image && (
+      {image && (
+        <div className="image-preview mb-4">
           <img src={image} alt="Uploaded" className="w-48 h-48 object-cover" />
-        )}
-      </div>
+        </div>
+      )}
+
       <button
         onClick={handlesubmit}
-        className="bg-blue-500 text-white p-2 rounded"
+        className="bg-blue-500 text-white p-2 rounded mt-4"
       >
-        Submit for Recongnition
+        Submit for Recognition
       </button>
 
       {result && (
-        <div className=" result mb-4">
-          <h3>Recongnition result</h3>
-          <p> {result.descripton}</p>
+        <div className="result flex flex-col items-center mt-4">
+          <h3 className="font-bold">Recognition result</h3>
+          <p>{result.description}</p>
         </div>
       )}
     </div>
   );
 }
+
 export default Homepage;
